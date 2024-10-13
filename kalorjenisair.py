e8a4ad5b3daa61d1070bba67197a9b00fc46ba1d
Resna Rahmawati <resnatic111@gmail.com>
2024-10-13 14:55:06 +0700

    latihan python

---
+++ kalorjenisair.py
@@ -0,0 +1,15 @@
+# KALOR JENIS AIR
+# Q = m × c air × t
+
+print('''
+=============================
+       KALOR JENIS AIR
+=============================
+''')
+
+m = float(input('Masukan massa benda:'))
+c_air = float(input('Masukan kalor jenis air (dalam Joule per kilogram per derajat Celsius) :'))
+t = float(input('Masukan perubahan suhu air:'))
+Q3 =  m * c_air * t
+
+print('KALOR JENIS AIR:',Q3)
