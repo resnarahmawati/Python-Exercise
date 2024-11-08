10231a5bf20835f0f6f48c3d6d89e5d87926266f
Resna Rahmawati <resnatic111@gmail.com>
2024-11-08 08:47:15 +0700

    Merapihkan

---
+++ Menentukan Sesuatu/2.Menentukan Point setiap pembelian 20ribu.py
@@ -0,0 +1,8 @@
+h1 = int(input("Masukkan Harga Barang 1 Rp:" ))
+h2 = int(input("Masukkan Harga Barang 1 Rp:" ))
+h3 = int(input("Masukkan Harga Barang 1 Rp:" ))
+
+s = h1+h2+h3
+p = s/20000
+
+print(f'Jumlah Point Adalah : {round(p,)}')
---
+++ ganjilgenap.py
@@ -0,0 +1,6 @@
+angka = int(input("Masukkan angka untuk menentukkan Ganjil/Genap: "))
+
+if angka %2 == 0:
+    print(f'Angka Tersebut Genap')
+else:
+    print("Angka Tersebut Ganjil")
