8b488e553fa19c9e857acbe24cc6ef7bb5cfc981
Resna Rahmawati <resnatic111@gmail.com>
2024-11-17 18:23:12 +0700

    Menuju 100 program

---
+++ Latihan dari algoritma munir/no12.py
@@ -0,0 +1,11 @@
+nilai = int(input('Masukkan nilai siswa: '))
+if 90 <= nilai <=100:
+    print('Grade A')
+elif 80 <= nilai <= 89:
+    print('Grade B')
+elif 70 <= nilai <= 79:
+    print('Grade C')
+elif 60 <= nilai <=69:
+    print('Grade D')
+elif nilai <= 59:
+    print('Grade E')
