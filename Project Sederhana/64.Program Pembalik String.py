8c5f4e5e2245491c92fe8bf66a6897715e74d0ce
= <=>
2024-12-17 18:14:03 +0700

    Program sederhana

---
+++ Project Sederhana/64.Program Pembalik String.py
@@ -0,0 +1,2 @@
+text = input("Masukkan teks: ")
+print(f"Teks terbalik: {text[::-1]}")
---
+++ Project Sederhana/65.Program Penjumlahan Dua Angka.py
@@ -0,0 +1,3 @@
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+print(f"Jumlah: {a + b}")
